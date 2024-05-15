aa0c98ef6837b32284367b81b80a976a5f99fdfa
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-14 23:05:30 -0700

    logo and navbar list

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <img src={logo} alt=''/>
+       <ul className="class-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>contact</li>
+       </ul>
+    </div>
   )
 }
 
