efb3d2dfcacca4c2edad07da906eac1cad170ef5
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-16 20:00:14 -0700

    added styling to hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
 const App = () => {
   return (
     <div><Navbar/>
-    <Hero/>
+      <Hero/>
     </div>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
 const Hero = () => {
   return (
-    <div className='hero'>Hero</div>
+    <div className='hero'>
+        <img src={profile_img}/>
+        <h1><span>I'm Padmaja Buggaveeti,</span> front end developer based in USA</h1>
+        <p>I'm Padmaja Buggaveeti, front end developer based in USA with 10 years of experience</p>
+        <div className='hero-action'>
+            <div className="hero-connect">Connect With Me</div>
+            <div className="hero-resume">My Resume</div>
+        </div>
+    </div>
   )
 }
 
