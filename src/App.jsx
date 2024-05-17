fc829a61b014cf18172aaf62bbf7858540e4295d
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-16 19:29:54 -0700

    Created hero component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 const App = () => {
   return (
-    <div><Navbar/></div>
+    <div><Navbar/>
+    <Hero/>
+    </div>
   )
 }
 
