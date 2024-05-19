ba7606f5848cf88a854c274aaea5a8afb1249c7d
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-19 16:03:50 -0700

    Added html content for contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+        <div className="contact-title">
+            <h1>Get in touch</h1>
+            <img src={theme_pattern}/>
+        </div>
+        <div className="contact-section">
+            <div className="contact-left">
+                <h1>Let's talk</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, obcaecati?</p>
+                <div className="contact-details">
+                    <div className="contact-detail">
+                        <img src={mail_icon} /><p>pbuggaveeti2811@gmail.com</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={call_icon} /><p>+1-206-290-5156</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={location_icon} /><p>Seattle, WA, United States</p>
+                    </div>
+                </div>
+            </div>
+            <form className="contact-right">
+                <label htmlFor="">Your Name</label>
+                <input type='text' placeholder='Enter your name' name='name'/>
+                <label htmlFor=''>Your Email</label>
+                <input type='email' placeholder='Enter your email' name='email'/>
+                <label htmlFor=''>Write your message here</label>
+                <textarea name='message' rows='8' placeholder='Enter your message'></textarea>
+                <button type='submit' className="contact-submit">Submit now</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
