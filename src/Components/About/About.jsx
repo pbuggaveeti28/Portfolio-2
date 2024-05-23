e9fc786123c95148da9aedad9807c5aa05e96c9e
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-22 23:02:02 -0700

    Added id's to all components

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,7 +5,7 @@ import theme_pattern from '../../assets/theme_pattern.svg'
 import profile_img from '../../assets/profile_img.svg'
 const About = () => {
   return (
-    <div className='about'>
+    <div id='about' className='about'>
         <div className="about-title">
             <h1>About Me</h1>
             <img src={theme_pattern}/>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
         <img src={profile_img}/>
         <h1><span>I'm Padmaja Buggaveeti,</span> front end developer based in USA</h1>
         <p>I'm Padmaja Buggaveeti, front end developer based in USA with 10 years of experience</p>
