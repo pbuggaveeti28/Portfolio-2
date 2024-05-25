5b12713561802474c4c7a0139956fc24e4f43eac
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-25 14:40:51 -0700

    Added access key

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,6 +6,31 @@ import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "7219f25f-b51e-4c70-a046-ce113677cbc3");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          console.log("Success", res);
+        }
+      };
+
+
   return (
     <div id='contact' className='contact'>
         <div className="contact-title">
@@ -28,7 +53,7 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <form className="contact-right">
+            <form onSubmit={onSubmit} className="contact-right">
                 <label htmlFor="">Your Name</label>
                 <input type='text' placeholder='Enter your name' name='name'/>
                 <label htmlFor=''>Your Email</label>
