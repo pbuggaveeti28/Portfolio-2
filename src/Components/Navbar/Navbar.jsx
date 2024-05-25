ff6d5c5542a206af2993ab5608c531e9b785c638
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-25 14:54:41 -0700

    added icons for responsiveness

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 import logo from '../../assets/logo.svg'
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_open from '../../assets/menu_open.svg'
+import menu_close from '../../assets/menu_close.svg'
 
 const Navbar = () => {
 
@@ -10,7 +12,9 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <img src={logo} alt=''/>
+        <img src={menu_open}/>
        <ul className="nav-menu">
+        <img className='nav-mob-close' src={menu_close}/>
         <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu=="home"?<img src={underline} alt=''/>:<></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")} >About Me</p></AnchorLink>{menu=="about"?<img src={underline} alt=''/>:<></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")} >Services</p></AnchorLink>{menu=="services"?<img src={underline} alt=''/>:<></>}</li>
